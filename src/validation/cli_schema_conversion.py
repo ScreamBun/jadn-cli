03a363cea146a357fce972bc0e788c92028cade0
Matt Roberts <matthew.roberts@bestgateeng.com>
2025-06-02 13:15:05 -0400

    added plantuml logic

--- src/validation/cli_schema_conversion.py
+++ src/validation/cli_schema_conversion.py
@@ -2,7 +2,7 @@ import json
 import jadn
 
 from jadnxml.builder.xsd_builder import convert_xsd_from_dict
-from src.utils.consts import JIDL_FILE_EXT, JSON_FILE_EXT, MARKDOWN_FILE_EXT, SCHEMAS_DIR_PATH, XSD_FILE_EXT
+from src.utils.consts import JIDL_FILE_EXT, JSON_FILE_EXT, MARKDOWN_FILE_EXT, PLANT_UML_FILE_EXT, SCHEMAS_DIR_PATH, XSD_FILE_EXT
 from src.utils.file_utils import get_file
 
 class CliSchemaConversion():
@@ -35,6 +35,14 @@ class CliSchemaConversion():
                 
             elif self.convert_to == MARKDOWN_FILE_EXT:
                 converted_schema = jadn.convert.markdown_dumps(schema_data)
+                
+            elif self.convert_to == PLANT_UML_FILE_EXT:
+                puml_style = jadn.convert.diagram_style()
+                puml_style['format'] = 'plantuml'
+                puml_style['detail'] = 'information'
+                puml_style['attributes'] = True
+                puml_style['enums'] = 100
+                converted_schema = jadn.convert.diagram_dumps(schema_data, puml_style)
                 
             elif self.convert_to == XSD_FILE_EXT:
                 result = convert_xsd_from_dict(schema_data)
